a80acc36dd5eeb1412a1c3e13c1643bce534c84f
Sonali2567 <satputesonali42@gmail.com>
2024-09-11 01:27:53 +0530

    AGAI CATEGORY CHANGES

--- src/app/pages/admin/products/products.component.ts
+++ src/app/pages/admin/products/products.component.ts
@@ -1,15 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProductService } from '../../../services/product/product.service';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
 isSidePanelVisible:boolean=false;
+productObj:any={
+    "productId": 0,
+    "productName": "",
+    "productPrice": 0,
+    "productDescription": "",
+    "productImageUrl": ""
+  
+}
+
+constructor(private productSrv:ProductService) {
+
+ }
+
 openSidelPanel(){
   this.isSidePanelVisible=true;
 }
