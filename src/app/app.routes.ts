8a73003c8b88246498e27dda5885118412fc7244
Sonali2567 <satputesonali42@gmail.com>
2024-09-12 12:39:38 +0530

    before starting add to cart

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginComponent } from './pages/login/login.component';
 //import { LayoutComponent } from './pages/layout/layout.component';
 import { ProductsComponent } from './pages/products/products.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
    
@@ -10,6 +11,10 @@ export const routes: Routes = [
        path:'login',
        component:LoginComponent 
        
+    },
+    {
+      path:'home',
+      component:HomeComponent
     },
     {
       path:'',
